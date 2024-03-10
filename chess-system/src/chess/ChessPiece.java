c7a3ebaf712de1051441b4a8684ad5949607989f
Kauã Hilton <kaua.hilton@gmail.com>
2024-03-09 22:32:48 -0300

    contagem de movimentos de peças

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -7,25 +7,36 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-	
-	
 
 	public Color getColor() {
 		return color;
 	}
 	
+	public int getMoveConunt() {
+		return moveCount;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreasseMoveCount() {
+		moveCount--;
+	}
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
-	
+
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
+
 }
--- chess-system/src/chess/ChessPosition.java
+++ chess-system/src/chess/ChessPosition.java
@@ -6,10 +6,10 @@ public class ChessPosition {
 
 	private char column;
 	private int row;
-	
+
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException(" Error ao instanciar ChessPosition. Os valores s�o de 'a1' a 'h8' ");
+			throw new ChessException(" Error ao instanciar ChessPosition. Os valores sao de 'a1' a 'h8' ");
 		}
 		this.column = column;
 		this.row = row;
@@ -19,7 +19,6 @@ public class ChessPosition {
 		return column;
 	}
 
-
 	public int getRow() {
 		return row;
 	}
@@ -27,14 +26,14 @@ public class ChessPosition {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
 	}
-	
+
 	@Override
 	public String toString() {
 		return "" + column + row;
 	}
-	
+
 }
